d9c17bc9fd3a21e1c048aa2b3770d42b2e303a46
Ramya Tripurana <navyadesires@gmail.com>
2022-01-20 19:40:02 +0530

    adding java testcases

---
+++ app.java
@@ -0,0 +1,13 @@
+package com.mouritech.jsonsample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
